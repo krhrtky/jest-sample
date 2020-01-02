0c0c9fe839a6564d914adf9c03092be7b9d78a04
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-02 16:43:39 +0900

    [add] toNumber test case. - fix jest config.

--- typescript/src/__test__/main.spec.ts
+++ typescript/src/__test__/main.spec.ts
@@ -1,10 +1,16 @@
-import { str } from '../main';
+import { str, toNumber } from '../main';
 
 describe('main', () => {
-  it('varable check', () => {
+  it('variable check', () => {
     expect(str).toEqual('sample');
   });
-  it('varable not equeal check', () => {
+  it('variable not equal check', () => {
     expect(str).not.toEqual('hoge');
+  });
+  it('args is numeric string', () => {
+    expect(toNumber('10')).toBe(10)
+  });
+  it('args is not numeric string', () => {
+    expect(toNumber('string')).toBe(0)
   });
 });
