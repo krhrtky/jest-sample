5ea0b6fc1e6d695089615a4ec4995c998dcf5c28
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-02 16:22:08 +0900

    [fix] typo.

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,1 +1,6 @@
-export const str: String = 'sample';
+export const str: string = 'sample';
+
+export const toNumber = (strNum: string): number => {
+  const converted = Number(strNum)
+  return isNaN(converted) ? 0 : converted
+}
