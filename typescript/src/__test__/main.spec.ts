4197347bff440e9e949f379a28dd06a154a18d35
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-02 10:10:36 +0900

    [add] not equeal case.

--- typescript/src/__test__/main.spec.ts
+++ typescript/src/__test__/main.spec.ts
@@ -2,6 +2,9 @@ import { str } from '../main';
 
 describe('main', () => {
   it('varable check', () => {
-    expect(str).toBe('sample');
+    expect(str).toEqual('sample');
+  });
+  it('varable not equeal check', () => {
+    expect(str).not.toEqual('hoge');
   });
 });
